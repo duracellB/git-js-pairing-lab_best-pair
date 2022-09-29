c9fd9b18df2ef3b068470dde3ed2e96412d0f69a
Swager0932 <swagerbot2000@gmail.com>
2022-09-29 18:11:00 +0000

    I solved #2

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ function fiveToOneHundred(){
     }
 }
 
-fiveToOneHundred()
+
+
+const multiplesOfThree = () => {
+    for(let i = 3; i <= 100; i++){
+        if (i%3===0){
+        console.log(i)
+    }
+    }    
+}
