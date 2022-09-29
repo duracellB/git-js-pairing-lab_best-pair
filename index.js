41de1f84d9e845326afd95d35afdc7a5d3687da2
duracellB <i.schlae.h@gmail.com>
2022-09-29 18:14:47 +0000

    India solved #3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Code your solutions in this file
+//1
 function fiveToOneHundred(){
     for(let i = 5; i<=100; i++){
         console.log(i)
     }
 }
 
-
-
+//2
 const multiplesOfThree = () => {
     for(let i = 3; i <= 100; i++){
         if (i%3===0){
@@ -14,3 +14,14 @@ const multiplesOfThree = () => {
     }
     }    
 }
+
+//3
+function multiplesOfThreeOrFive(){
+    for(let i = 3 ; i<=100 ; i+=1){
+        if(i%3 === 0 || i%5 === 0){
+            console.log(i)
+        }
+    }
+}
+
+//4
